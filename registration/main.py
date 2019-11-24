7347deea7c45635d04dea1fca3cf3ea499b13a03
Devilkessi <57586822+Devilkessi@users.noreply.github.com>
2019-11-24 18:36:28 +1200

    🥚

--- registration/main.py
+++ registration/main.py
@@ -3,18 +3,25 @@ from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
 # class SignInScreen(Screen):
 #    pass
 Window.size = (480, 853)
 
-class Container(GridLayout):
+class Container(BoxLayout):
+    layout = BoxLayout(spacing=10)
+    btn1 = Button(text='Hello', size_hint=(.7, 1))
+    btn2 = Button(text='World', size_hint=(.3, 1))
+    layout.add_widget(btn1)
+    layout.add_widget(btn2)
     pass
 
 
 class MainApp(App):
     def build(self):
         return Container()
+
 
 if __name__ == '__main__':
     MainApp().run()
