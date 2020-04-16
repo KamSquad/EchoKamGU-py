997faffacd2fb7630f99f5171104031d94fa050d
starky41 <maxim333rrr@gmail.com>
2020-04-16 22:04:31 +1200

    :sparkles: Added settings and notifications screens twice :sparkles:

---
+++ settings/settings.py
@@ -0,0 +1,14 @@
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import Screen
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivymd.app import MDApp
+
+
+class SettingsScreen(Screen):
+    pass
+
+
+
